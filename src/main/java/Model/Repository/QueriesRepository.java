e07b3e5e947440de2b508f5394ca1c561ecb46be
TaliZorah <orossbogdan@gmail.com>
2016-04-13 22:09:28 +0300

    Database tests added

--- src/main/java/Model/Repository/QueriesRepository.java
+++ src/main/java/Model/Repository/QueriesRepository.java
@@ -1,11 +1,15 @@
 package Model.Repository;
 
 import Model.Entity.QueriesEntity;
+import Model.Entity.TypeOfQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Created by xoll on 11.04.16.
  */
 public interface QueriesRepository extends JpaRepository<QueriesEntity, Integer> {
     QueriesEntity getQueriesByIdQuery(int id);
+    List<QueriesEntity> getQueriesByType(TypeOfQuery typeOfQuery);
 }
